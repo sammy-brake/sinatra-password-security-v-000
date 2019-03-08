13fe8113a1fa6257e26c630d64287cd03037146d
Samantha brake <sammy.brake@gmail.com>
2019-03-08 16:07:44 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -44,7 +44,7 @@ post "/login" do
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-      redirect to "/account"
+      redirect to "/success"
     else
       redirect to "/failure"
     end
